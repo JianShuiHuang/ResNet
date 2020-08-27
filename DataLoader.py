6ddd6c1404d5c417c3347b328b0560782dc94e22
JianShuiHuang <abcdefg101074@gmail.com>
2020-08-27 13:33:50 +0800

    first commit

---
+++ DataLoader.py
@@ -0,0 +1,72 @@
+import torch
+import numpy as np
+import pandas as pd
+from torch.utils import data
+from PIL import Image
+from torchvision import transforms
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def getData(mode):
+    if mode == 'train':
+        img = pd.read_csv('train_img.csv')
+        label = pd.read_csv('train_label.csv')
+        return np.squeeze(img.values), np.squeeze(label.values)
+    else:
+        img = pd.read_csv('test_img.csv')
+        label = pd.read_csv('test_label.csv')
+        return np.squeeze(img.values), np.squeeze(label.values)
+
+
+class DataLoader(data.Dataset):
+    def __init__(self, root, mode, transform = None):
+        """
+        Args:
+            root (string): Root path of the dataset.
+            mode : Indicate procedure status(training or testing)
+
+            self.img_name (string list): String list that store all image names.
+            self.label (int or float list): Numerical list that store all ground truth label values.
+        """
+        self.root = root
+        self.img_name, self.label = getData(mode)
+        self.mode = mode
+        print("> Found %d images..." % (len(self.img_name)))
+
+    def __len__(self):
+        """'return the size of dataset"""
+        return len(self.img_name)
+
+    def __getitem__(self, index):
+        """something you should implement here"""
+
+        """
+           step1. Get the image path from 'self.img_name' and load it.
+                  hint : path = root + self.img_name[index] + '.jpeg'
+           
+           step2. Get the ground truth label from self.label
+                     
+           step3. Transform the .jpeg rgb images during the training phase, such as rssh ubuntu@140.113.215.195 -p porttc. But at the beginning, I suggest you follow the hints. 
+                       
+                  In the testing phase, if you have a normalization process during the training phase, you only need 
+                  to normalize the data. 
+                  
+                  hints : Convert the pixel value to [0, 1]
+                          Transpose the image shape from [H, W, C] to [C, H, W]
+                         
+            step4. Return processed image and label
+        """
+        ##step1
+        path = self.root + self.img_name[index] + '.jpeg'
+        img = Image.open(path)
+        img = img.resize((64, 64),Image.ANTIALIAS
+        
+        ##step2
+        GroundTruth = 1 if self.label[index] >= 1 else 0
+        
+        ##step3
+        img_np = np.asarray(img)/255
+        img_np = np.transpose(img_np, (2,0,1))
+        img_ten = torch.from_numpy(img_np)
+        
+        ##step4
+        return img_ten, GroundTruth
---
+++ Main.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  2 09:30:21 2020
+
+@author: ivis
+"""
+
+import torch
+import torch.nn as nn
+import numpy as np
+import torch.optim as optim
+from torch.autograd import Variable
+import torchvision.models as models 
+import pandas as pd
+from torch.utils import data
+from PIL import Image
+from torchvision import transforms
+from ResNet import *
+from DataLoader import *
+from Time import *
+from TrainTest import *
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+##Hyper parameters
+lr = 1e-03
+BatchSize = 4
+Epochs18 = 10
+Epochs50 = 5
+Momentum = 0.9     
+Weight_decay = 5e-4
+
+def main():
+    transformations = transforms.Compose([transforms.ToTensor()])
+    train_data = DataLoader('data/', 'train', transformations)
+    test_data = DataLoader('data/', 'test', transformations)
+    train_dataloader = torch.utils.data.DataLoader(dataset=train_data, batch_size=BatchSize, shuffle=False)
+    test_dataloader = torch.utils.data.DataLoader(dataset=test_data, batch_size=BatchSize, shuffle=False)  
+    
+   
+    ##pretrain resnet18    
+    train_accuracy = []
+    test_accuracy = []
+    
+    model = Pretrained_model18()
+    #model = torch.load('preresnet18.pkl').to(device)
+    
+    model = model.to(device)
+    optimizer = optim.SGD(model.parameters(), lr = lr, momentum = Momentum, weight_decay = Weight_decay)
+    
+    for i in range(Epochs18):
+        train = Train(train_dataloader, model, optimizer)
+        train_accuracy.append(train)
+        test = Test(test_dataloader, model, Epochs18)
+        test_accuracy.append(test)
+        print("epochs:", i )
+        print('Train Accuracy: ', train)
+        print('Test Accuracy: ', test)
+    print('Max accuracy: ', max(test_accuracy))
+    print("resnet18 complet...")
+
+    """
+    ##pretrain resnet50
+    train_accuracy0 = []
+    test_accuracy0 = []
+    
+    model0 = Pretrained_model50()
+    #model0 = torch.load('preresnet50.pkl').to(device)
+    
+    model0 = model0.to(device)
+    optimizer0 = optim.SGD(model0.parameters(), lr = lr, momentum = Momentum, weight_decay = Weight_decay)
+    
+    for i in range(Epochs50):
+        train0 = Train(train_dataloader, model0, optimizer0)
+        train_accuracy0.append(train0)
+        test0 = Test(test_dataloader, model0, optimizer0, Epochs50, i)
+        test_accuracy0.append(test0)
+        print("epochs:", i )
+        print('Train Accuracy: ', train0)
+        print('Test Accuracy: ', test0)
+    print('Max accuracy: ', max(test_accuracy0))
+    print("resnet50 complet...")
+    """
+    
+main()    
