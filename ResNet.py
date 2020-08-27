011c40ea33b9f5d82bd638b4e9cd8eb81050a944
JianShuiHuang <47372304+JianShuiHuang@users.noreply.github.com>
2020-08-27 14:06:41 +0800

    Update ResNet.py

--- ResNet.py
+++ ResNet.py
@@ -6,12 +6,12 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def Pretrained_model18():
     resnet18 = models.resnet18(pretrained = False)
-    resnet18.fc = nn.Linear(in_features=512, out_features=5, bias=True)
+    resnet18.fc = nn.Linear(in_features=64, out_features=2, bias=True)
     
     return resnet18
 
 def Pretrained_model50():
     resnet50 = models.resnet50(pretrained = False)
-    resnet50.fc = nn.Linear(in_features=2048, out_features=5, bias=True)
+    resnet50.fc = nn.Linear(in_features=2048, out_features=2, bias=True)
     
     return resnet50
